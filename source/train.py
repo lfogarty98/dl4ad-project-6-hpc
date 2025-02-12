01fec4e0af279264d6bbaa43e842ad739321e130
lfogarty98 <lfogarty9995@gmail.com>
2025-02-12 21:43:18 +0000

    dvc: commit experiment b600ca80b86bc004ea6686dd3bbef5f4198bf2f5668b6e8d08c1be80a86a42ab

--- source/train.py
+++ source/train.py
@@ -64,16 +64,18 @@ def generate_predictions(model, device, dataloader, num_eval_batches):
             y = y.to(device)
             predicted_batch = model(X)
             
-            prediction = torch.cat((prediction, predicted_batch.flatten()), 0)
-            target = torch.cat((target, y.flatten()), 0)
+            prediction = torch.cat((prediction, predicted_batch), 0)
+            target = torch.cat((target, y), 0)
     # Convert prediction and target from normalized proll to plots
-    piano_roll_prediction_plot = plot_binarized_piano_roll(prediction.cpu().numpy(), "Predicted Piano Roll")
-    piano_roll_target_plot = plot_binarized_piano_roll(target.cpu().numpy(), "Target Piano Roll")
+    prediction = prediction.cpu().numpy().squeeze() 
+    target = target.cpu().numpy().squeeze()
+    piano_roll_prediction_plot = plot_binarized_piano_roll(prediction, "Predicted Piano Roll")
+    piano_roll_target_plot = plot_binarized_piano_roll(target, "Target Piano Roll")
     return piano_roll_prediction_plot, piano_roll_target_plot
 
 def plot_binarized_piano_roll(piano_roll, plot_title):
     piano_roll_scaled = (piano_roll * 127).astype(int) # Scale back to [0, 127]
-    ppr_object = ppr.Multitrack(tracks=[ppr.StandardTrack(pianoroll=piano_roll_scaled.T)]) # Transpose to match pypianoroll format
+    ppr_object = ppr.Multitrack(tracks=[ppr.StandardTrack(pianoroll=piano_roll_scaled)]) # Transpose to match pypianoroll format
     ppr_object.binarize()
     fig, ax = plt.subplots(figsize=(12, 6))
     ppr.plot_pianoroll(ax, ppr_object.tracks[0].pianoroll, cmap="Blues")
