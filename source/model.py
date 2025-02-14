806297ee155c0d89d7e9cffdc73492073b907cd5
Liam Fogarty <lfogarty9995@gmail.com>
2025-02-14 14:06:55 +0100

    add sigmoid function for model output

--- source/model.py
+++ source/model.py
@@ -6,8 +6,10 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         self.lstm = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim, num_layers=num_lstm_layers, batch_first=True, bias=True)
         self.linear = nn.Linear(in_features=hidden_dim, out_features=output_dim)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x, (hidden, cell) = self.lstm(x)
         x = self.linear(x)
+        x = self.sigmoid(x)  # Ensure output is in [0,1] range, since target proll values are normalized to this range
         return x
