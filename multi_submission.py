ea6d0f917e6b87396e4e303e94a68b400bde3454
Liam Fogarty <lfogarty9995@gmail.com>
2025-03-19 02:24:18 +0000

    dvc: commit experiment 63e13db7da02f0b79b726752e6999f848f4f30cc044d807066aeda16a9607c73

--- multi_submission.py
+++ multi_submission.py
@@ -10,12 +10,12 @@ import os
 import sys
 
 # Submit experiment for hyperparameter combination
-def submit_batch_job(arguments, test_split, batch_size):
+def submit_batch_job(arguments, hidden_size, num_lstm_layers):
     # Set dynamic parameters for the batch job as environment variables
     # But dont forget to add the os.environ to the new environment variables otherwise the PATH is not found
     env = {
         **os.environ,
-        "EXP_PARAMS": f"-S preprocess.test_split={test_split} -S train.batch_size={batch_size}"
+        "EXP_PARAMS": f"-S model.hidden_size={hidden_size} -S model.num_lstm_layers={num_lstm_layers}"
     }
     # Run sbatch command with the environment variables as bash! subprocess! command (otherwise module not found)
     subprocess.run(['/usr/bin/bash', '-c', f'sbatch slurm_job.sh {" ".join(arguments)}'], env=env)
@@ -24,8 +24,8 @@ if __name__ == "__main__":
 
     arguments = sys.argv[1:]
 
-    test_split_list = [0.2, 0.3]
-    batch_size_list = [2048, 4096]
+    hidden_size_list = [500, 750, 1000]
+    num_lstm_layers_list = [3, 5, 7]
     # Iterate over a cartesian product parameter grid of the test_split and batch_size lists
-    for test_split, batch_size in itertools.product(test_split_list, batch_size_list):
-        submit_batch_job(arguments, test_split, batch_size)
+    for hidden_size, num_lstm_layers in itertools.product(hidden_size_list, num_lstm_layers_list):
+        submit_batch_job(arguments, hidden_size, num_lstm_layers)
